449628cb6873c39a86e15a22406669a97b8b913c
usrnm242 <k3nt242@gmail.com>
2020-06-06 17:31:07 +0300

    new server addr

--- file.py
+++ file.py
@@ -1,35 +1,52 @@
 import json
 import requests
+import datetime
 
-with open("conf.json", 'r') as confFile:
-    conf = confFile.read()
+
+# with open("conf.json", 'r') as confFile:
+#     conf = confFile.read()
+
+start = datetime.datetime.now()
+conf = requests.get("https://usrnm242.github.io/getcoupon/conf.json").text
+stop = datetime.datetime.now()
+measurement = stop - start
+
+print(f"conf: {measurement.seconds}.{measurement.microseconds} seconds")
 
 conf = json.loads(conf)
 
 iosJson = None
 
-for server in conf['servers']:
-    serverAddr = server['serverAddress']
-    iosJsonPath = server['iosJson']
-    androidJsonPath = server['androidJson']
-    adsPath = server['ads'] if server['ads'] != "" else None
+server = conf['defaultServer']
 
-    iosJsonAddr = f"{serverAddr}{iosJsonPath}"
-    androidJsonAddr = f"{serverAddr}{androidJsonPath}"
+serverAddr = server['serverAddress']
+iosJsonPath = server['iosJson']
+androidJsonPath = server['androidJson']
+adsPath = server['ads'] if server['ads'] != "" else None
 
-    try:
-        iosRequest = requests.get(iosJsonAddr)
-    except Exception:
-        continue
+iosJsonAddr = f"{serverAddr}{iosJsonPath}"
+androidJsonAddr = f"{serverAddr}{androidJsonPath}"
 
-    if not iosRequest or iosRequest.status_code != 200:
-        # 200 is 'ok'
-        continue
-    else:
-        iosJson = json.loads(iosRequest.text)
-        break
+try:
+    start = datetime.datetime.now()
+    iosRequest = requests.get(iosJsonAddr)
+    stop = datetime.datetime.now()
+    measurement = stop - start
+    print(f"json: {measurement.seconds}.{measurement.microseconds} seconds")
+except Exception as e:
+    raise(e)
 
-if not iosJson:
-    iosJson = "LOAD JSON FROM CACHE"
+if not iosRequest:
+    raise AttributeError("not iosRequest")
+elif iosRequest.status_code != 200:
+    raise AttributeError(f"status: {iosRequest.status_code}")
+else:
+    iosJson = json.loads(iosRequest.text)
 
-print(iosJson)
+assert iosJson
+
+req = requests.get(f"{serverAddr}{adsPath}")
+assert req
+ads = req.text
+print(ads)
+# print(iosJson)
